df8277cb155e1e910eb7772dadc6433c7b9b6e1f
Jeyym Santos <55918108+jeyymsantos@users.noreply.github.com>
2023-02-18 01:38:32 +0800

    API Goods for Login Customer and Get Order

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -20,9 +21,44 @@ class LoginController extends Controller
     public function GetProducts(){
         $products = DB::table('products')
         ->select('*')
-        ->where('prod_status', '=', 'Available')
         ->get();
 
         return response()->json($products, 200, [], JSON_PRETTY_PRINT);
     }
+
+    public function LoginCustomer(Request $req){
+
+        $email = $req->email;
+        $password = $req->password;
+
+        if(is_null($email) || is_null($password)){
+            echo 'All fields required!';
+            return;
+        };
+
+        $user = DB::table('customers')
+        ->select('*')
+        ->join('users', 'users.id', '=', 'customers.cust_id')
+        ->where('email', '=', $email)
+        ->get()->first();
+
+        if(Hash::check($password, $user->password)){
+            return response()->json($user, 200, [], JSON_PRETTY_PRINT);
+        }
+
+        echo 'Account not found';
+        
+    }
+
+    public function GetOrder($id){
+        $order = DB::table('orders')
+        ->select('*')
+        ->join('customers', 'customers.cust_id', '=', 'orders.cust_id')
+        ->join('users', 'customers.cust_id', '=', 'users.id')
+        ->orderBy('order_date', 'desc')
+        ->where('orders.cust_id', '=',$id)->get();
+
+        return response()->json($order, 200, [], JSON_PRETTY_PRINT);
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// General or Bulk Objects
 Route::get('customers', [LoginController::class, 'GetCustomers']);
 Route::get('products', [LoginController::class, 'GetProducts']);
+
+// Post Objects (login pending)
+Route::post('login', [LoginController::class, 'LoginCustomer']);
+
+Route::get('order/{id}', [LoginController::class, 'GetOrder']);
