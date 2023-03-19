2185d982bf7a932935a6f5ba7ac451b84b1e06d0
Jeyym Santos <55918108+jeyymsantos@users.noreply.github.com>
2023-03-19 14:50:38 +0800

    Sample APKKKKKK

--- resources/views/layouts/landing_page.blade.php
+++ resources/views/layouts/landing_page.blade.php
@@ -48,8 +48,9 @@
                 <ul>
                     <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
                     <li><a class="nav-link scrollto" href="#about">About</a></li>
-                    <li><a class="nav-link" href="https://we.tl/t-FMaIg7K6Rs" target="_blank">Download APK </a></li>
+                    <li><a class="nav-link" href="https://we.tl/t-FMaIg7K6Rs" target="_blank">Download APKKKKKKKKKK </a></li>
                     {{-- <li><a class="nav-link scrollto" href="#portfolio">Products</a></li> --}}
+
 
                     @guest
                         @if (Route::has('register'))
