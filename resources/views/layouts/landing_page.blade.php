085091323af1b69229c018eefd7a8ba10736fc0c
Jeyym Santos <55918108+jeyymsantos@users.noreply.github.com>
2023-04-14 23:05:49 +0800

    Updated subdomain files

--- resources/views/layouts/landing_page.blade.php
+++ resources/views/layouts/landing_page.blade.php
@@ -39,7 +39,7 @@
     <header id="header" class="header fixed-top">
         <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-            <a href="index.html" class="logo d-flex align-items-center">
+            <a href="/" class="logo d-flex align-items-center">
                 <img src="{{ asset('img/Logo.png') }}" alt="">
                 <span>Mine Ditse</span>
             </a>
